cf1608a01d1af757c392ecdb06c73c34b4b0ca71
Ethan Stewart <ethan@atdllc.com>
2014-01-13 17:07:07 -0500

    Pre-Charlie push

--- js/js.js
+++ js/js.js
@@ -1,5 +1,40 @@
+var teams;
+var matches;
+
+var uploadStack;
+
 
 function onld(){
-  alert("Hi");
+  
+//  alert("Hi");
+//  document.getElementsByClassName('addMatch')[0].style.display = "block";
 }
 
+function windowclose(){
+  
+}
+
+
+function newMatch(){
+  
+}
+
+function newTeam(){
+  
+}
+
+function setMatch(){
+  
+}
+
+function setTeam(){
+  
+}
+
+function dissapear(){
+  active.style.display = "none";
+}
+
+// addMatch;
+// addTeam;
+// optionsTitle;
