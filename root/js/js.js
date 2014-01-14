14e4531931bc667210342ba746ea035f6b509c4a
Chaos <cawiii@me.com>
2014-01-13 19:20:23 -0500

    charlie's view switcher

--- root/js/js.js
+++ root/js/js.js
@@ -3,6 +3,38 @@ var matches;
 
 var uploadStack;
 
+var pageView;
+
+$(document).ready(function(){
+    pageView = 1;
+
+    $(document).resize(function(){
+        switch(pageView){
+        default:
+        case 1: // home
+            $('#addTeam').css('visibility', 'hidden');
+            $('#addMatch').css('visibility', 'hidden');
+            break;
+        case 2: // addTeam
+            $('#addTeam').css('visibility', 'visible');
+            $('#addMatch').css('visibility', 'hidden');
+            break;
+        case 3: // addMatch
+            $('#addTeam').css('visibility', 'hidden');
+            $('#addMatch').css('visibility', 'visible');
+            break;
+        }
+    });
+
+    $('#addTeamButton').click(function(){
+        pageView = 2;
+        $(document).resize();
+    });
+    $('#addMatchButton').click(function(){
+        pageView = 3;
+        $(document).resize();
+    });
+});
 
 function onld(){
   
