9302f117cec54b8115e729b930fa378590ee9b9f
Ethan Stewart <ethan@atdllc.com>
2014-01-13 19:04:30 -0500

    Push after charlie

--- root/js/js.js
+++ root/js/js.js
@@ -3,11 +3,22 @@ var matches;
 
 var uploadStack;
 
+var showID;
 
 function onld(){
   
 //  alert("Hi");
 //  document.getElementsByClassName('addMatch')[0].style.display = "block";
+  
+  showID = 0;
+  show();
+  
+  document.getElementsByClassName('addTeam')[0].style.display = "block";
+  document.getElementById("0").style.display = "block";
+}
+
+function show(){
+  document.getElementById(showID).style.display = "block";
 }
 
 function windowclose(){
