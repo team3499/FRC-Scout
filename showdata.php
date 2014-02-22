ccab0e775cab9e713ad69017db1fea6ed02675bd
Ethan Stewart <ethan@atdllc.com>
2014-02-22 15:38:29 -0500

    Changed the colors on the view page to be less intrusive

--- showdata.php
+++ showdata.php
@@ -5,22 +5,22 @@
         
         <table id="tableTeams">
             <tr>
-                <td class="tableTeamsTeam"><strong>Team</strong></td>
-                <td class="tableTeamsCatch"><strong>Catch?</strong></td>
-                <td class="tableTeamsPickup"><strong>Pickup?</strong></td>
-                <td class="tableTeamsThrow"><strong>Throw?</strong></td>
-                <td class="tableTeamsAuto"><strong>Auto?</strong></td>
-                <td class="tableTeamsNotes"><strong>Notes</strong></td>
-                <td class="tableTeamsTotalPoints"><strong>Total Points</strong></td>
+                <td class="blackBG tableTeamsTeam"><strong>Team</strong></td>
+                <td class="blackBG tableTeamsCatch"><strong>Catch?</strong></td>
+                <td class="blackBG tableTeamsPickup"><strong>Pickup?</strong></td>
+                <td class="blackBG tableTeamsThrow"><strong>Throw?</strong></td>
+                <td class="blackBG tableTeamsAuto"><strong>Auto?</strong></td>
+                <td class="blackBG tableTeamsNotes"><strong>Notes</strong></td>
+                <td class="blackBG tableTeamsTotalPoints"><strong>Total Points</strong></td>
             </tr>
             <tr>
-                <td class="tableTeamsTeam" title="Team Number"><a href="team.php?team=3499"><strong>0001</strong></a></td>
+                <td class="blackBG tableTeamsTeam" title="Team Number"><a href="team.php?team=3499"><strong>0001</strong></a></td>
                 <td class="tableTeamsCatch greenBG" title="Catch?">Yes</td>
                 <td class="tableTeamsPickup yellowBG" title="Pickup?">Maybe <span title="The team said that they could pickup during the interview, but in game, they do not!">(?)</span></td>
                 <td class="tableTeamsThrow greenBG" title="Throw?">Yes</td>
                 <td class="tableTeamsAuto redBG" title="Auto?">no</td>
                 <td class="tableTeamsNotes yellowBG" title="Team Notes"><span title="blah blah blah blah blah blah blah blah There is a lot of notes to test this field">blah blah blah blah blah blah blah blah There is a lot of notes to test this field</span></td>
-                <td class="tableTeamsTotalPoints" title="Total Points">99999</td>
+                <td class="blackBG tableTeamsTotalPoints" title="Total Points">99999</td>
             </tr>            
         
         </table>
@@ -31,7 +31,7 @@
         
         <table id="tableMatches">
             <tr>
-               <td class="tableMatchesMatch"><strong>Match #</strong></td>
+               <td class="blackBG tableMatchesMatch"><strong>Match #</strong></td>
                <td class="tableMatchesBlueTeam blueBG"><strong>B Team #1</strong></td>
                <td class="tableMatchesBlueTeam blueBG"><strong>B Team #2</strong></td>
                <td class="tableMatchesBlueTeam blueBG"><strong>B Team #3</strong></td>
@@ -40,19 +40,19 @@
                <td class="tableMatchesRedTeam redBG"><strong>R Team #3</strong></td>
                <td class="tableMatchesBlueScore blueBG"><strong>Blue Team Score</strong></td>
                <td class="tableMatchesRedScore redBG"><strong>Red Team Score</strong></td>
-               <td class="tableMatchesNotes"><strong>Match Notes</strong></td>
-               <td class="tableMatchesWin"><strong>Team Winner</strong></td>
+               <td class="blackBG tableMatchesNotes"><strong>Match Notes</strong></td>
+               <td class="blackBG tableMatchesWin"><strong>Team Winner</strong></td>
             </tr>
             <tr>
-               <td class="tableMatchesMatch" title="Match Number"><a href="#001">001</a></td>
-               <td class="tableMatchesBlueTeam blue" title="Blue Team #1"><a href="#0001">0001</a></td>
-               <td class="tableMatchesBlueTeam blue" title="Blue Team #2"><a href="#0002">0002</a></td>
-               <td class="tableMatchesBlueTeam blue" title="Blue Team #3"><a href="#0003">0003</a></td>
-               <td class="tableMatchesRedTeam red" title="Red Team #1"><a href="#0004">0004</a></td>
-               <td class="tableMatchesRedTeam red" title="Red Team #2"><a href="#0005">0005</a></td>
-               <td class="tableMatchesRedTeam red" title="Red Team #3"><a href="#0006">0006</a></td>
-               <td class="tableMatchesBlueScore blue" title="Blue Team Score">3213</td>
-               <td class="tableMatchesRedScore red" title="Red Team Score">3646</td>
+               <td class="blackBG tableMatchesMatch" title="Match Number"><a href="#001">001</a></td>
+               <td class="blackBG tableMatchesBlueTeam blue" title="Blue Team #1"><a href="#0001">0001</a></td>
+               <td class="blackBG tableMatchesBlueTeam blue" title="Blue Team #2"><a href="#0002">0002</a></td>
+               <td class="blackBG tableMatchesBlueTeam blue" title="Blue Team #3"><a href="#0003">0003</a></td>
+               <td class="blackBG tableMatchesRedTeam red" title="Red Team #1"><a href="#0004">0004</a></td>
+               <td class="blackBG tableMatchesRedTeam red" title="Red Team #2"><a href="#0005">0005</a></td>
+               <td class="blackBG tableMatchesRedTeam red" title="Red Team #3"><a href="#0006">0006</a></td>
+               <td class="blackBG tableMatchesBlueScore blue" title="Blue Team Score">3213</td>
+               <td class="blackBG tableMatchesRedScore red" title="Red Team Score">3646</td>
                <td class="tableMatchesNotes yellowBG" title="Match Ntoes"><span title="There is a lot of notes to test this field">There is a lot of notes to test this field</span></td>
                <td class="tableMatchesWin blueBG" title="Team Winner">Blue</td>
             </tr>
